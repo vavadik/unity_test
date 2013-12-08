5e281104f980437143a41882abb0a8ccf72873f3
vadim <vadim.galanov@gmail.com>
2013-12-09 01:33:56 +0200

    + add all

---
+++ Assets/Scripts/MainPlane.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainPlane : MonoBehaviour {
+
+	List<GameObject> nodes;
+
+	// Use this for initialization
+	void Start () {
+		nodes = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		Event e = Event.current;
+		if (e.button == 0 && e.isMouse) {
+			if(Input.GetMouseButtonUp(0)){
+				Debug.Log("Left Click");
+				AddNode();
+			}
+		} else if (e.button == 1 && e.isMouse) {
+			if(Input.GetMouseButtonUp(1)){
+				Debug.Log("Right Click");
+			}
+		}
+	}
+
+	void AddNode() {
+		GameObject node = (GameObject) Instantiate(Resources.Load("Circle"));
+		this.nodes.Add (node);
+	}
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Vector3 speed = new Vector3 (mousePos.x - transform.position.x, mousePos.y - transform.position.y, 0).normalized;
+		transform.position += speed * Time.deltaTime;
+	}
+}
